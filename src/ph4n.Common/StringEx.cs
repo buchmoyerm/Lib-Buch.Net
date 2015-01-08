d087b47b8f2ced192b301bbc0cf01814e8e2cbd2
Mark Buchmoyer <buchmoyerm@gmail.com>
2015-01-08 16:21:33 -0500

    [add] string/char functions

---
+++ src/ph4n.Common/StringEx.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ph4n.Common
+{
+    public static class StringEx
+    {
+        public static string SafeSubstring(this string input, int startIndex, int length)
+        {
+            if (startIndex < 0 || length < 0)
+            {
+                return string.Empty;
+            }
+
+            if (input.Length >= (startIndex + length))
+            {
+                return input.Substring(startIndex, length);
+            }
+            else
+            {
+                if (input.Length > startIndex)
+                {
+                    return input.Substring(startIndex);
+                }
+                else
+                {
+                    return string.Empty;
+                }
+            }
+        }
+    }
+
+    public static class CharEx
+    {
+        public static bool IsVulgarFraction(this char c)
+        {
+            //checks against the hex values of the vulgar fractions
+            //see http://en.wikipedia.org/wiki/Number_Forms for values
+            return (c >= 0x00BC && c <= 0x00BE) ||
+                   (c >= 0x2150 && c <= 0x215E);
+        }
+
+        public static double VulgarCharToDec(this char c)
+        {
+            double d = 0.0;
+
+            switch (Convert.ToInt64(c))
+            {
+                case 0x00BC:
+                    d = .25;
+                    break;
+                case 0x00BD:
+                    d = .5;
+                    break;
+                case 0x00BE:
+                    d = .75;
+                    break;
+                case 0x2150:
+                    d = 1 / 7;
+                    break;
+                case 0x2151:
+                    d = 1 / 9;
+                    break;
+                case 0x2152:
+                    d = .1;
+                    break;
+                case 0x2153:
+                    d = 1 / 3;
+                    break;
+                case 0x2154:
+                    d = 2 / 3;
+                    break;
+                case 0x2155:
+                    d = .2;
+                    break;
+                case 0x2156:
+                    d = .4;
+                    break;
+                case 0x2157:
+                    d = .6;
+                    break;
+                case 0x2158:
+                    d = .8;
+                    break;
+                case 0x2159:
+                    d = 1 / 6;
+                    break;
+                case 0x215A:
+                    d = 5 / 6;
+                    break;
+                case 0x215B:
+                    d = .125;
+                    break;
+                case 0x215C:
+                    d = .375;
+                    break;
+                case 0x215D:
+                    d = .625;
+                    break;
+                case 0x215E:
+                    d = .875;
+                    break;
+            }
+
+            return d;
+        }
+    }
+}
---
+++ test/ph4n.Test/Common/StringExTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ph4n.Common;
+
+namespace ph4n.Test.Common
+{
+    [TestClass]
+    public class StringExTests
+    {
+        [TestMethod]
+        public void SafeSubstring_begining()
+        {
+            var str = "this is a normal string";
+            var substr = "this";
+            Assert.AreEqual(substr, str.SafeSubstring(0,4), "Did not return correct substring");
+        }
+
+        [TestMethod]
+        public void SafeSubstring_middle()
+        {
+            var str = "01234string5678";
+            var substr = "string";
+            Assert.AreEqual(substr, str.SafeSubstring(5, 6), "Did not return the correct substring");
+        }
+
+        [TestMethod]
+        public void SafeSubstring_end_valid()
+        {
+            var str = "01234string";
+            var substring = "string";
+            Assert.AreEqual(substring, str.SafeSubstring(5,6), "Did not return the correct substring");
+        }
+
+        [TestMethod]
+        public void SafeSubstring_end_pastend()
+        {
+            var str = "01234string";
+            var substring = "string";
+            Assert.AreEqual(substring, str.SafeSubstring(5, 100), "Did not return the correct substring");
+        }
+
+        [TestMethod]
+        public void SafeSubstring_start_pastend()
+        {
+            var str = "01234";
+            Assert.AreEqual(string.Empty, str.SafeSubstring(100,200), "Did not an empty string");
+        }
+
+        [TestMethod]
+        public void SafeSubstring_negative_startindex_returns_empty()
+        {
+            var str = "01234";
+            Assert.AreEqual(string.Empty, str.SafeSubstring(-1, 200), "Did not an empty string");
+        }
+    }
+}
