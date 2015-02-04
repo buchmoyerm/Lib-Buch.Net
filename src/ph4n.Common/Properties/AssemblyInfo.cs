608a67682f0eb1793c60961067cdfe2d44ba944e
Mark Buchmoyer <buchmoyerm@gmail.com>
2015-02-04 09:49:54 -0500

    [remove] remaining traces of Buch.Net

--- src/ph4n.Common/Properties/AssemblyInfo.cs
+++ src/ph4n.Common/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Buch.Net.Common")]
+[assembly: AssemblyTitle("ph4n.Common")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Buch.Net.Common")]
+[assembly: AssemblyProduct("ph4n.Common")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- src/ph4n.Threading/Properties/AssemblyInfo.cs
+++ src/ph4n.Threading/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Threading")]
+[assembly: AssemblyTitle("ph4n.Threading")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Threading")]
+[assembly: AssemblyProduct("ph4n.Threading")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
