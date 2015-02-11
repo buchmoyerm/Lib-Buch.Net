7a958383589488831c175376fcb59eab219b9354
Mark Buchmoyer <buchmoyerm@gmail.com>
2015-02-11 09:45:04 -0500

    [refactor] more descriptive name [fix] interlocked increments and have one index for pushing and one for popping (for race conditions, pop index will only get updated AFTER there is something to pop)

--- src/ph4n.Containers/LocklessFixedSizeStack.cs
+++ src/ph4n.Containers/LocklessFixedSizeStack.cs
@@ -9,13 +9,14 @@ using ph4n.Common;
 
 namespace ph4n.Containers
 {
-    public class LocklessStack<T>
+    public class LocklessFixedSizeStack<T>
     {
         private T[] _array;
-        private int _top = -1;
+        private int _pushTo = -1;
+        private int _popFrom = 0;
         private int _capacity;
 
-        public LocklessStack(int capacity)
+        public LocklessFixedSizeStack(int capacity)
         {
             Validate.ArgumementGreaterThan(capacity, 0, "capacity");
 
@@ -28,26 +29,34 @@ namespace ph4n.Containers
             Validate.ArgumentNotNull(item,"item");
 
             //increment when not at the top
-            var pushLocation = Interlocked.CompareExchange(ref _top, _top + 1, _capacity);
+            var pushLocation = Interlocked.Increment(ref _pushTo);
             if ( pushLocation >= 0 && pushLocation < _capacity)
             {
                 _array[pushLocation] = item;
+                Interlocked.Increment(ref _popFrom);
                 return true;
             }
-
+            
+            //could not push, undo uncrement
+            Interlocked.Decrement(ref _pushTo);
             return false;
         }
 
         [CanBeNull]
         public T TryPop()
         {
-            //decrement when not at the bottom
-            var popLocation = Interlocked.CompareExchange(ref _top, _top - 1, -1);
+            //decrement the popFrom
+            var popLocation = Interlocked.Decrement(ref _popFrom);
             if (popLocation < _capacity && popLocation > -1)
             {
-                return _array[popLocation];
+                var ret = _array[popLocation];
+                _array[popLocation] = default(T); //remove reference to item so it can be garbage collected
+                Interlocked.Decrement(ref _pushTo);
+                return ret;
             }
 
+            //could not pop, undo decrement
+            Interlocked.Increment(ref _popFrom);
             return default(T);
         }
     }
--- test/ph4n.Test/Containers/LocklessFixedSizeStackTest.cs
+++ test/ph4n.Test/Containers/LocklessFixedSizeStackTest.cs
@@ -5,7 +5,7 @@ using ph4n.Containers;
 namespace ph4n.Test.Containers
 {
     [TestClass]
-    public class LocklessStackTest
+    public class LocklessFixedSizeStackTest
     {
         class Foo
         {
@@ -16,27 +16,27 @@ namespace ph4n.Test.Containers
         [TestMethod]
         public void valid_ctor_test()
         {
-            var stack = new LocklessStack<Foo>(10);
+            var stack = new LocklessFixedSizeStack<Foo>(10);
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentOutOfRangeException))]
         public void invalid_ctor_test()
         {
-            var stack = new LocklessStack<Foo>(0);
+            var stack = new LocklessFixedSizeStack<Foo>(0);
         }
 
         [TestMethod]
         public void TryPush_onto_empty_stack()
         {
-            var stack = new LocklessStack<Foo>(2);
+            var stack = new LocklessFixedSizeStack<Foo>(2);
             Assert.IsTrue(stack.TryPush(new Foo()), "Indicates could not push");
         }
 
         [TestMethod]
         public void TryPush_onto_full_stack()
         {
-            var stack = new LocklessStack<Foo>(1);
+            var stack = new LocklessFixedSizeStack<Foo>(1);
             Assert.IsTrue(stack.TryPush(new Foo()), "Could not push first item");
             Assert.IsFalse(stack.TryPush(new Foo()), "Should not be able to push past capacity");
         }
@@ -44,7 +44,7 @@ namespace ph4n.Test.Containers
         [TestMethod]
         public void TryPop_from_empty_stack()
         {
-            var stack = new LocklessStack<Foo>(2);
+            var stack = new LocklessFixedSizeStack<Foo>(2);
             Assert.IsNull(stack.TryPop(), "Did not pop null");
         }
 
@@ -52,7 +52,7 @@ namespace ph4n.Test.Containers
         public void TryPop_first_item()
         {
             var f = new Foo() {TestStr = "test"};
-            var stack = new LocklessStack<Foo>(2);
+            var stack = new LocklessFixedSizeStack<Foo>(2);
             stack.TryPush(f);
             Assert.AreEqual(f, stack.TryPop(), "Popped item is not the same as pushed");
         }
@@ -62,7 +62,7 @@ namespace ph4n.Test.Containers
         {
             var first = new Foo() {TestStr = "first"};
             var second = new Foo() {TestStr = "second"};
-            var stack = new LocklessStack<Foo>(2);
+            var stack = new LocklessFixedSizeStack<Foo>(2);
 
             stack.TryPush(first);
             stack.TryPop();
